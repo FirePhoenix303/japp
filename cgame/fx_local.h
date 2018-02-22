932811bbc460ad9e0ae6cba71d7ace286738b5fd
Fire Phoenix <palpaphoenix@gmail.com>
2018-02-22 10:20:27 +0100

    Update fx_local.h

--- cgame/fx_local.h
+++ cgame/fx_local.h
@@ -51,3 +51,9 @@ void FX_RocketProjectileThink( centity_t *cent, const struct weaponInfo_s *weapo
 void FX_RocketAltProjectileThink( centity_t *cent, const struct weaponInfo_s *weapon );
 void FX_RocketHitWall( vector3 *origin, vector3 *normal );
 void FX_RocketHitPlayer( vector3 *origin, vector3 *normal, qboolean humanoid );
+
+// Clone
+void FX_CloneProjectileThink( centity_t *cent, const struct weaponInfo_s *weapon );
+void FX_CloneAltFireThink( centity_t *cent, const struct weaponInfo_s *weapon );
+void FX_CloneWeaponHitWall( vec3_t origin, vec3_t normal );
+void FX_CloneWeaponHitPlayer( vec3_t origin, vec3_t normal, qboolean humanoid );
